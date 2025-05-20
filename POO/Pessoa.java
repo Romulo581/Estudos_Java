ec005a4edc0c0f0d9fe7afd8eeab03f8299ae00e
Romulo581 <romulo.da508@gmail.com>
2025-05-19 21:22:31 -0300

    aulas de hoje

--- POO/Pessoa.java
+++ POO/Pessoa.java
@@ -1,5 +1,6 @@
 package POO;
 // Novos objetos para poder fazer ações no programa com elesss
+
 // interações entre objetos
 
 //Classe -> objeto
@@ -7,49 +8,68 @@ package POO;
 //Cria um novo objeto = instancia
 
 public class Pessoa {
- 
-    
+
     // Atributos = Caracteristicas
     private String nome;
     private int idade;
     private double altura;
     private double peso;
 
-    public Pessoa(){
+    // função construtora
+    public Pessoa() {
         this.nome = "Sem nome";
         this.idade = 0;
         this.altura = 0.0;
         this.peso = 0.0;
     }
-    //Métodos -> ações
-    // setters e getters
-    // setters para colocar, atribuir valor
-    // getters para pegar 
-    public void setNome(String nome){
+
+    // função construtora com atributos
+    public Pessoa(String nome, int idade, double altura, double peso) {
         this.nome = nome;
-    }
-    public String getNome(){
-        return this.nome;
-    }
-    //idade
-    public void setIdade(int idade){
         this.idade = idade;
-    }
-    public int getIdade(){
-        return this.idade;
-    }
-    //altura
-    public void setAltura(double altura){
         this.altura = altura;
-    }
-    public double getAltura(){
-        return this.altura;
-    }
-    //peso
-    public void setPeso(double peso){
         this.peso = peso;
     }
-    public double getPeso(){
+
+    // Métodos -> ações
+    // setters e getters
+    // setters para colocar, atribuir valor
+    // getters para pegar
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    // idade
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getIdade() {
+        return this.idade;
+    }
+
+    // altura
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double getAltura() {
+        return this.altura;
+    }
+
+    // peso
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getPeso() {
         return this.peso;
+    }
+    public double calcularImc(){
+        return peso / (altura * altura);
     }
 }
--- POO/ProgramaPrincipal.java
+++ POO/ProgramaPrincipal.java
@@ -13,5 +13,12 @@ public class ProgramaPrincipal {
         pessoa1.setAltura(1.75);
 
         System.out.println("Meu nome é " + pessoa1.getNome() + " eu tenho " + pessoa1.getIdade() + " anos, peso por volta de " + pessoa1.getPeso() + "Kg e tenho " + pessoa1.getAltura() + " metros de Altura");
+
+
+        Pessoa pessoa2 = new Pessoa( "César", 27, 68,1.75);
+
+        System.out.println("Olá meu nome é " + pessoa2.getNome() + " eu tenho " + pessoa2.getIdade() + " de idade," + " tenho cerca de " + pessoa2.getPeso() + "kg e tenho " + pessoa2.getAltura() + " metros de altura");
+
+        System.out.println(pessoa2.calcularImc());
     }
 }
